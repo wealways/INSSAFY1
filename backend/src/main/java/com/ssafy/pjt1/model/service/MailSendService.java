9db9fdbb5e120e6f535db32d5491ecfa8c88f405
DESKTOP-KVCQHCD\multicampus <sumin9497@naver.com>
2021-01-25 14:25:54 +0900

    기능변경:MailSenderService 수정

---
+++ backend/src/main/java/com/ssafy/pjt1/model/service/MailSendService.java
@@ -0,0 +1,63 @@
+package com.ssafy.pjt1.model.service;
+
+import java.util.Random;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailSendService {
+	
+	@Autowired
+    private JavaMailSenderImpl mailSender;
+
+	int size;
+    //인증키 생성
+    private String getKey(int size) {
+        this.size = size;
+        return getAuthCode();
+    }
+
+    //인증코드 난수 발생
+    private String getAuthCode() {
+        Random random = new Random();
+        StringBuffer buffer = new StringBuffer();
+        int num = 0;
+
+        while(buffer.length() < size) {
+            num = random.nextInt(10);
+            buffer.append(num);
+        }
+
+        return buffer.toString();
+    }
+
+    //인증메일 보내기
+    public String sendAuthMail(String email) {
+        //6자리 난수 인증번호 생성
+        String authKey = getKey(6);
+
+        MimeMessage mail = mailSender.createMimeMessage();
+        String mailContent = "<h1>[이메일 인증]</h1><br><p>아래 링크를 클릭하시면 이메일 인증이 완료됩니다.</p>"
+                            + "<a href='http://localhost:8000/acount/joinConfirm?email=" 
+                            + email + "&authKey=" + authKey + "' target='_blenk'>이메일 인증 확인</a>";
+
+        try {
+            mail.setSubject("회원가입 이메일 인증 ", "utf-8");
+            mail.setText(mailContent, "utf-8", "html");
+            mail.addRecipient(Message.RecipientType.TO, new InternetAddress(email));
+            mailSender.send(mail);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
+          return authKey;
+    }
+}
+
