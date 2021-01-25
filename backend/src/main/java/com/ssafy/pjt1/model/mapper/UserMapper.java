c737ff39a52551131c767163b4baf9e0bc046c8c
moonjinh <moonjinh123@naver.com>
2021-01-25 15:05:01 +0900

    버그: 내정보 보기

---
+++ backend/src/main/java/com/ssafy/pjt1/model/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.ssafy.pjt1.model.mapper;
+
+import com.ssafy.pjt1.model.dto.user.UserDto;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+    public UserDto login(UserDto userDto);
+
+    public int join(UserDto userDto);
+
+    public UserDto userInfo(String user_email);
+}
---
+++ backend/src/main/java/com/ssafy/pjt1/model/service/UserServiceImpl.java
@@ -0,0 +1,34 @@
+package com.ssafy.pjt1.model.service;
+
+import com.ssafy.pjt1.model.dto.user.UserDto;
+import com.ssafy.pjt1.model.mapper.UserMapper;
+
+import org.apache.ibatis.session.SqlSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private SqlSession sqlSession;
+
+    public static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
+
+    @Override
+    public UserDto login(UserDto userDto) {
+        return sqlSession.getMapper(UserMapper.class).login(userDto);
+    }
+
+    @Override
+    public boolean join(UserDto userDto) {
+        return sqlSession.getMapper(UserMapper.class).join(userDto) == 1;
+    }
+
+    @Override
+    public UserDto userInfo(String user_email) {
+        return sqlSession.getMapper(UserMapper.class).userInfo(user_email);
+    }
+}
